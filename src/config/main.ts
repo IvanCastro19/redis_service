f8923908538b8e6db0fc3f6b3b32f6523924a068
IvanCastro19 <79170316+IvanCastro19@users.noreply.github.com>
2022-11-30 16:43:47 -0600

    update

--- src/config/main.ts
+++ src/config/main.ts
@@ -1,9 +1,14 @@
 import Bull from 'bull';
 import config from './constants/configs';
+import { BullAdapter } from 'bull-board/bullAdapter';
+import { createBullBoard } from 'bull-board';
 
 const name: string = 'messages';
 
-export const Queue = () => new Bull(name, {
-    redis: { host: config.REDIS.HOST, port: parseInt(config.REDIS.PORT) }
+export const Queue = new Bull(name, {
+    redis: config.REDIS.URL
 });
 
+export const { router } = createBullBoard([
+    new BullAdapter(Queue)
+]);
