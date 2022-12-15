c97ea45d31c73e3d974509ad9bfe8585297ef5b6
IvanCastro19 <ivancastromartinezkk@gmail.com>
2022-12-15 13:08:29 -0600

    new tests and catch errors in add jobs function

--- src/controllers/redis.messages.ts
+++ src/controllers/redis.messages.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { addMessageJob } from '../database/bull/bull.controllers';
 
 export const storeMessage = async (req: Request, res: Response): Promise<Response> => {
-    if(!req.body.contacts)
+    if(!req.body.contacts || !req.body.contacts[0].wa_id || req.body.contacts[0].wa_id.length < 13 || !req.body.messages[0])
         return res.status(400).json({status: 'error', message: 'No data provided'});
         
     try {
--- src/database/bull/bull.controllers.ts
+++ src/database/bull/bull.controllers.ts
@@ -2,16 +2,25 @@ import { Queue } from '../../config/bull';
 
 const options = {
     backoff: 5000,
-    attempts: 3
+    attempts: 10
 };
 
 export const addMessageJob = (message: any) => {
     return new Promise<boolean>( async (resolve, reject) => {
-        await Queue.add({ job: message }, options)
-        Queue.on('error', (error: any) => {
-            console.error(error);
-            resolve(false);
+        let done: boolean = true;
+        let error: string | null = null;
+
+        Queue.add({ job: message }, options);
+        Queue.on('error', (e: any) => {
+            error = e;
+            done = false;
         });
-        resolve(true);
+
+        setTimeout(() => {
+            if(error)
+                console.log(error)    
+            
+            resolve(done)
+        }, 10);
     })
 };
